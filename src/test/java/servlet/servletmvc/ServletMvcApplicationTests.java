ed9322e00bbe6a29e018c1de6e06de6cd4326aa6
jay sung <namic123@naver.com>
2024-01-24 02:12:06 +0900

    Spring MVC basic project first commit

---
+++ src/test/java/servlet/servletmvc/ServletMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package servlet.servletmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
