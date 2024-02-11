4314d812c92739e7c9996310fdfb607e7d50b95f
jay sung <namic123@naver.com>
2024-02-11 20:33:48 +0900

    Enumeration, 구 방식의 http 헤드 정보 불러오기

--- src/main/java/servlet/servletmvc/basic/request/RequestHeaderServlet.java
+++ src/main/java/servlet/servletmvc/basic/request/RequestHeaderServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
@@ -15,6 +16,7 @@ public class RequestHeaderServlet extends HttpServlet {
         printStartLine(request);
     }
 
+    // Http Start line 정보
     private static void printStartLine(HttpServletRequest request) {
         System.out.println("--- REQUEST-LINE - start ---");
         System.out.println("request.getMethod() = " + request.getMethod()); // HTTP 메서드
@@ -31,5 +33,18 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.isSecure() = " + request.isSecure()); //https 사용 유무
         System.out.println("--- REQUEST-LINE - end ---");
         System.out.println();
+    }
+
+    // Header 모든 정보
+    private void printHeaders(HttpServletRequest request){
+        System.out.println("--- HTTP 헤더 로그 시작 ---");
+
+        // 옛날 방식의 헤드 정보 가져오는 방법
+        // Enumeration을 통해 반복하여 각각의 헤더를 순회
+        Enumeration<String> headerNames = request.getHeaderNames(); // 헤더의 이름 리스트 모두 호출
+        while (headerNames.hasMoreElements()){  // 각각의 헤더들을 순회
+            String headerName = headerNames.nextElement();  // 요소 저장
+            System.out.println(headerName + ": " + request.getHeader(headerName));
+        }
     }
 }
