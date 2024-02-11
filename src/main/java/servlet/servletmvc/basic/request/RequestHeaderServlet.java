213ff8228745d140a9760c38efa02cb001f40017
jay sung <namic123@naver.com>
2024-02-11 20:29:41 +0900

    요청 Start line 정보 확인 기본 메서드

---
+++ src/main/java/servlet/servletmvc/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,35 @@
+package servlet.servletmvc.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        printStartLine(request);
+    }
+
+    private static void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + request.getMethod()); // HTTP 메서드
+        System.out.println("request.getProtocol() = " + request.getProtocol()); // HTTP 버전
+
+        System.out.println("request.getScheme() = " + request.getScheme()); // http
+        // http://localhost:8080/request-header
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        // /request-header
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        //username=hi
+        System.out.println("request.getQueryString() = " +
+                           request.getQueryString()); // 쿼리 스트링
+        System.out.println("request.isSecure() = " + request.isSecure()); //https 사용 유무
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+}
