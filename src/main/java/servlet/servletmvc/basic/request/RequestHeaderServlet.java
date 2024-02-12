f2604b7c7e56658827eb9ce5efd91aea510cd354
jay sung <namic123@naver.com>
2024-02-12 19:37:12 +0900

    최근에 많이 사용되는 헤더 정보 가져오는 법

--- src/main/java/servlet/servletmvc/basic/request/RequestHeaderServlet.java
+++ src/main/java/servlet/servletmvc/basic/request/RequestHeaderServlet.java
@@ -14,6 +14,7 @@ public class RequestHeaderServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         printStartLine(request);
+        printHeaders(request);
     }
 
     // Http Start line 정보
@@ -39,12 +40,18 @@ public class RequestHeaderServlet extends HttpServlet {
     private void printHeaders(HttpServletRequest request){
         System.out.println("--- HTTP 헤더 로그 시작 ---");
 
-        // 옛날 방식의 헤드 정보 가져오는 방법
-        // Enumeration을 통해 반복하여 각각의 헤더를 순회
-        Enumeration<String> headerNames = request.getHeaderNames(); // 헤더의 이름 리스트 모두 호출
-        while (headerNames.hasMoreElements()){  // 각각의 헤더들을 순회
-            String headerName = headerNames.nextElement();  // 요소 저장
-            System.out.println(headerName + ": " + request.getHeader(headerName));
-        }
+//        // 옛날 방식의 헤드 정보 가져오는 방법
+//        // Enumeration을 통해 반복하여 각각의 헤더를 순회
+//        Enumeration<String> headerNames = request.getHeaderNames(); // 헤더의 이름 리스트 모두 호출
+//        while (headerNames.hasMoreElements()){  // 각각의 헤더들을 순회
+//            String headerName = headerNames.nextElement();  // 요소 저장
+//            System.out.println(headerName + ": " + request.getHeader(headerName));
+//        }
+
+request.getHeaderNames().asIterator()
+        .forEachRemaining(headerName -> System.out.println(headerName+": "+ headerName));
+
+        System.out.println("--- Headers - end ---");
+        System.out.println();
     }
 }
